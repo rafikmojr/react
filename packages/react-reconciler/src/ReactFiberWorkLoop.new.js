e9ae20abf502cbefcf12592a80e3fd7227cf3f4d
Rick Hanlon <rickhanlonii@gmail.com>
2022-05-25 15:28:18 -0400

    Timeslice in the rAF same as Default Lane

--- packages/react-reconciler/src/ReactFiberWorkLoop.new.js
+++ packages/react-reconciler/src/ReactFiberWorkLoop.new.js
@@ -908,9 +908,7 @@ function performConcurrentWorkOnRoot(root, didTimeout) {
   const shouldTimeSlice =
     !includesBlockingLane(root, lanes) &&
     !includesExpiredLane(root, lanes) &&
-    (disableSchedulerTimeoutInWorkLoop || !didTimeout) &&
-    enableFameEndScheduling &&
-    root.frameAlignedNode !== null;
+    (disableSchedulerTimeoutInWorkLoop || !didTimeout);
   let exitStatus = shouldTimeSlice
     ? renderRootConcurrent(root, lanes)
     : renderRootSync(root, lanes);
